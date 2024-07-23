d2104b9266437aa408af56efeb1aece178cfc022
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-23 21:56:33 +0000

    Jul 23, 2024, 6:56 PM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import { Container, ThemeProvider } from "@mui/material";
-import { theme } from "./theme";
-import AddTask from "./components/addTask";
+import { Container, ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "./theme"; // Certifique-se de que a exportação está correta
+import AddTask from "./components/AddTask"; 
 
 export default function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<Container>
+			<CssBaseline />
+			<Container maxWidth="sm" sx={{ padding: 2 }}>
 				<AddTask />
 			</Container>
 		</ThemeProvider>
