39ffd5082a84947cfdb3ec86a57812f4d32f265d
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-23 21:52:13 +0000

    Jul 23, 2024, 6:52 PM

---
+++ frontend/src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#000000",
+		},
+		secondary: {
+			main: "#000000",
+		},
+	},
+	typography: {
+		fontFamily: "Roboto, Arial, sans-serif",
+	},
+});
+
+export default theme;
