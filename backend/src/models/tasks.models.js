bbb8605ad8330acc556d7b8cb59d4841168c1e4d
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-23 21:06:56 +0000

    Jul 23, 2024, 6:06 PM

--- backend/src/models/tasks.models.js
+++ backend/src/models/tasks.models.js
@@ -5,6 +5,10 @@ const tasksSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	description: {
+		type: String,
+		required: true,
+	},
 	completed: {
 		type: Boolean,
 		default: false,
