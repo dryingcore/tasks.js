7b18346e9da52b45b307a677c75b96d1b5b3d428
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-23 22:17:49 +0000

    Jul 23, 2024, 7:17 PM

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -1,10 +1,14 @@
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 
 export default function AddTask() {
 	return (
 		<Box component="form" sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
 			<TextField label="Title" variant="outlined" required />
 			<TextField label="Description" variant="outlined" multiline rows={4} required />
+
+			<Button variant="contained" type="submit">
+				Add Task
+			</Button>
 		</Box>
 	);
 }
