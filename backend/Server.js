cb96bfdf33410b36b914c90826615e805f18d0e3
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-23 19:03:27 +0000

    Jul 23, 2024, 4:03 PM

--- backend/Server.js
+++ backend/Server.js
@@ -1,2 +1,17 @@
-const express = require("express");
-const app = express();
+const App = require("./src/App");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 3333;
+
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log("Connected to MongoDB");
+		App.listen(PORT, () => {
+			console.log(`Server running on port ${PORT}`);
+		});
+	})
+	.catch(err => {
+		console.log(err);
+	});
