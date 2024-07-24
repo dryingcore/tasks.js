8b5f03d6837c49523dea7e272751d25dcb05f748
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-24 13:31:01 +0000

    Jul 24, 2024, 10:31 AM

--- backend/src/App.js
+++ backend/src/App.js
@@ -3,6 +3,11 @@ const cors = require("cors");
 const App = express();
 const tasksRoute = require("./routes/tasks.route");
 
+App.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
 App.use(cors());
 App.use(express.json());
 App.use("/api/tasks", tasksRoute);
